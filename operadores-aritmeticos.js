5ced39fa3381827c15b68c360ce9191e5292b238
Evellyn de Oliveira <108280573+evypersonal@users.noreply.github.com>
2024-03-14 14:13:57 +0000

    Criando um arquivo contendo operadores aritméticos

---
+++ operadores-aritmeticos.js
@@ -0,0 +1,13 @@
+//selecionar todas as lets para const CTRL + D
+const num1 = 40;
+const num2 = 20;
+
+//Quando declarar variável no texto é necessário utilizar o acento grave ``
+//Duplicar linha Alt + Shift + Seta
+console.log(`Os números das operações são ${num1} e ${num2}`);
+console.log(`A soma dos números é ${num1 + num2}`);
+console.log(`A subtração dos números é ${num1 - num2}`);
+console.log(`A multiplicação dos números é ${num1 * num2}`);
+console.log(`A divisão dos números é ${num1 / num2}`);
+console.log(`O resto da divisão dos números é ${num1 % num2}`);
+//Executar -> node operadores-aritmeticos.js
