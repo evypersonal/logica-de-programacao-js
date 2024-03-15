8702427abf990e1f6e8179170d4f4e12e96eb98f
Evellyn de Oliveira <108280573+evypersonal@users.noreply.github.com>
2024-03-15 14:49:50 +0000

    Adicionando os comparadores lógicos

---
+++ operadores-logicos.js
@@ -0,0 +1,17 @@
+const a = false, b = false;
+
+if (a || b){
+    console.log('TRUE');
+}else{
+    console.log('FALSE');
+}
+
+if (a && b){
+    console.log('TRUE');
+}else {
+    console.log('FALSE');
+}
+
+// ||   -> OU; (uma das variaveis terá que ser verdadeira);
+// &&   -> E; (ambas variaveis deverão ser verdadeiras);
+// !    -> NÃO;
