290444f69af428d576bd263d193bd2201bbf17d7
Evellyn de Oliveira <108280573+evypersonal@users.noreply.github.com>
2024-03-15 14:51:35 +0000

    Desafio IMC (@TODO)

---
+++ desafio2.js
@@ -0,0 +1,19 @@
+const peso = window.prompt('Digite seu peso: ');
+const altura = window.prompt('Digite sua altura:');
+const mult = altura * 2;
+const imc = mult / peso;
+// const imc = peso / altura * 2;
+
+if(imc < 18.5){
+    window.alert('Abaixo do peso');
+}else if(imc == 18.5 || imc <= 24.9){
+    window.alert('Peso normal');
+}else if(imc == 25 || imc <= 29.9){
+    window.alert('Sobrepeso');
+}else if(imc == 30 || imc <= 34.9){
+    window.alert('Obesidade Grau I');
+}else if(imc == 35 || imc <= 39.9){
+    window.alert('Obesidade Grau II');
+}else{
+    window.alert('Obesidade Grau III');
+}
